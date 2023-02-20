9f8494bfde66584de2b10eeb39f3b1489dc9d0ea
AlexChen208 <alex.chencs207@gmail.com>
2023-02-20 15:31:35 -0800

    updating sample code with django imports

--- code.py
+++ code.py
@@ -1,16 +1,20 @@
-def registration_business(request):
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+
+def registration_view(request):
   context = {}
   if request.POST:
-    form = RegistrationBusinessForm(request.POST)
+    form = RegistrationForm(request.POST)
     if form.is_valid():
       form.save()
-      user = request.user.id
-      business_id = form.instance.id
-      UserForm.objects.get(id=user).business.add(business_id)
-      return redirect('construct_email')
+      email = form.cleaned_data.get('email')
+      raw_password = form.cleaned_data.get('password1')
+      account = authenticate(email = email, password = raw_password)
+      login(request, account)
+      return redirect('post_signup')
     else:
-      context['registration_business_form'] = form
+      context['registration_form'] = form
   else:
-    form = RegistrationBusinessForm()
-    context['registration_business_form'] = form
-  return render(request, 'registration/business1.html', context)
+    form = RegistrationForm()
+    context['registration_form'] = form
+  return render(request, 'registration/signup.html', context)
